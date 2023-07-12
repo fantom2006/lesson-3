bd5e744b016afb53a59a8453199eb61860b93ad6
fantom2006 <alibaeveldiar01@gmail.com>
2023-07-12 17:42:27 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,21 @@
+/* дз 1
+let first = prompt('Введите число')
+let second = prompt('Введите число')
+
+if(first > second){
+    alert(second + ' меньше чем ' + first)
+}else if(second > first){
+    alert(first + ' меньше чем ' + second)
+}else{
+    alert("Они равны")
+}
+*/
+//дз 2
+/*
+let res = (function() {
+    let a = prompt('Введите число')
+    let b = prompt('Введите степень числа')
+    return a ** b
+}) ()
+alert(res)
+*/
